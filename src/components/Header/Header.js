0f8044786f0180c8e0fe82bae466d3d27a1efcaa
werivik <wervik00994@stud.noroff.no>
2025-03-03 13:43:30 +0100

    making a responsive menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useShoppingCart } from "../../pages/ShoppingCart/ShoppingCart";
 import styles from "./Header.module.css";
 import shoppingBag from '/media/icons/shoppingbag.png';
+import openMenu from '/media/icons/openmenu.png';
+import defaultMenu from '/media/icons/defaultmenu.png';
 
 function Header() {
   const { getTotalItems } = useShoppingCart();
   const totalItems = getTotalItems();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <header className={styles.header}>
@@ -21,6 +29,27 @@ function Header() {
                 <img src={shoppingBag} alt="Shopping Bag" className={styles.cart} />
               </Link>
             </div>
+          </li>
+        </ul>
+
+        <div className={styles.menuIcon} onClick={toggleMenu}>
+          <img src={menuOpen ? openMenu : defaultMenu} alt="Menu" />
+        </div>
+
+        <Link to="/" className={styles.logo2}>Nuvra</Link>
+
+        <ul className={`${styles.responsiveMenu} ${menuOpen ? styles.showMenu : ""}`}>
+          <li className={styles.responsiveLinks}> 
+            <Link to="/" className={styles.respoLink}>Home</Link>
+            <Link to="/products" className={styles.respoLink}>Products</Link>
+            <Link to="/ShoppingCart" className={styles.respoLink}>
+              <div className={styles.respoCart}>
+                <span>Shopping Cart</span>
+                <p style={{ opacity: totalItems > 0 ? 1 : 0 }}>{totalItems}</p>
+                <img src={shoppingBag} alt="Shopping Bag" className={styles.cart} />
+              </div>
+            </Link>
+            <Link to="/Contact" className={styles.respoLink}>Contact Us</Link>
           </li>
         </ul>
       </nav>
